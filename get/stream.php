eec385aaee738fc241b72fb5fffd1536fedba78a
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-06-15 17:44:37 +0200

    change default portal to https://skyportal.xyz

--- get/stream.php
+++ get/stream.php
@@ -40,7 +40,7 @@ if (!$stmt->execute([$streamid])) {
 if (isset($_GET['portal']) && !empty($_GET['portal'])) {
     $portal = filter_var($_GET['portal'], FILTER_SANITIZE_URL);
 } else {
-    $portal = 'https://helsinki.siasky.net';
+    $portal = 'https://skyportal.xyz';
 }
 
 
