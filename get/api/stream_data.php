631ac83ed92478716b227a0786616dceffa55b9b
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-06-18 19:58:27 +0200

    return 404 if stream_data not found

--- get/api/stream_data.php
+++ get/api/stream_data.php
@@ -10,4 +10,9 @@ require 'model/get_stream.php';
 $stream_data = get_stream_data_from_token($_GET['token']);
 
 header('Content-Type: application/json');
-echo json_encode($stream_data);
+if (!empty($stream_data)) {
+    echo json_encode($stream_data);
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo json_encode(array("error" => "404 :ot found"));
+}
