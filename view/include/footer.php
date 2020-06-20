eefc492ba8dccc227b666c575c36df3b2639bcc5
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-06-20 14:40:57 +0200

    built with Sia fix #35

--- view/include/footer.php
+++ view/include/footer.php
@@ -5,6 +5,9 @@
             <a href="https://minnit.chat/SkyLive" class="text-white">Chat</a>
             •
             <a href="https://github.com/DaWe35/SkyLive" class="text-white">GitHub</a>
+            •
+            <a href="https://sia.tech"><img src="assets/logos/built-with-sia-color.png" class="built-with-sia-logo" alt="built with sia" /></a>
+            
         </p>
     </div>
     <!-- /.container -->
