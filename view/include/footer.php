71ae27d5f6c67cef30e02e9fd232cb58a716d383
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-10-17 23:41:35 +0200

    change chat url to https://napchat.hns.siasky.net/#main

--- view/include/footer.php
+++ view/include/footer.php
@@ -2,7 +2,7 @@
 <footer class="py-5 <?= PAGE != 'player' ? 'bg-dark' : '' ?>">
     <div class="container">
         <p class="m-0 text-center text-white">
-            <a href="https://minnit.chat/SkyLive" class="text-white">Chat</a>
+            <a href="https://napchat.hns.siasky.net/#main" class="text-white">Decentralized chat</a>
             •
             <a href="https://github.com/DaWe35/SkyLive" class="text-white">GitHub</a>
             •
