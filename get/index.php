f0302447841209f38492ab25b306c48fd9eae120
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-11-07 20:27:28 +0100

    hide stream if not started after scheule_time

--- get/index.php
+++ get/index.php
@@ -1,8 +1,11 @@
 <?php
 
-$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE visibility = 'public' ORDER BY scheule_time DESC, streamid DESC LIMIT 100");
-if (!$streams_stmt->execute()) {
+$current_time_plus_10_min = time() - 1800;
+
+$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE visibility = 'public' AND (started = 1 OR scheule_time > ?) ORDER BY scheule_time DESC, streamid DESC LIMIT 100");
+if (!$streams_stmt->execute([$current_time_plus_10_min])) {
     exit('Database error');
 }
 
 include 'model/display.php';
+
