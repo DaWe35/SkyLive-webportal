e6a3daf472eb02495f5e9122ea951f02551f7bce
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-11-11 19:49:00 +0100

    exclude channels

--- get/index.php
+++ get/index.php
@@ -2,7 +2,7 @@
 
 $current_time_plus_10_min = time() - 1800;
 
-$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE visibility = 'public' AND (started = 1 OR scheule_time > ?) ORDER BY scheule_time DESC, streamid DESC LIMIT 100");
+$streams_stmt = $db->prepare("SELECT `streamid`, `title`, `description`, `scheule_time`, `started`, `finished` FROM `stream` WHERE userid NOT IN (80, 90) AND visibility = 'public' AND (started = 1 OR scheule_time > ?) ORDER BY scheule_time DESC, streamid DESC LIMIT 100");
 if (!$streams_stmt->execute([$current_time_plus_10_min])) {
     exit('Database error');
 }
